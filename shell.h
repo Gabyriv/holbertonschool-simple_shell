95fdf3fa4129fbb7639cb6c69bcbbfa05c1d1790
sigidibau <9571@holbertonstudents.com>
2024-08-19 15:08:09 -0400

    header file

---
+++ shell.h
@@ -0,0 +1,44 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <time.h>
+
+extern char **environl;
+
+int exec(char **arguments);
+int exist(char *pathname);
+int _putchar(char c);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
