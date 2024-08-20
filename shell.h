8367d8939b07a1a26e78ca70baad68a537ba7da3
sigidibau <9571@holbertonstudents.com>
2024-08-20 15:05:28 -0400

    added more function proto

--- shell.h
+++ shell.h
@@ -15,8 +15,10 @@ extern char **environl;
 int exec(char **arguments);
 int exist(char *pathname);
 int _putchar(char c);
-
-
+void prompt(void);
+int _strlen(const char *string);
+char *_strdup(char *str);
+int _strcmp(char *str1, char *str2);
 
 
 
