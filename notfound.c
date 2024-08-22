82613c80a5ee68577f9eb806ca9545860f9c4023
sigidibau <9571@holbertonstudents.com>
2024-08-22 11:20:48 -0400

    fix betty problems

--- notfound.c
+++ notfound.c
@@ -1,13 +1,13 @@
 #include "shell.h"
 
 /**
- *
- *
- *
- *
+ *not_found - puts an error message when command is not found
+ *@args: array of args typed by the user
+ *@counter: count the time the shell was use
+ *Return: Exit status
  */
 
-int not_found(char **arguments, int counter)
+int not_found(char **args, int counter)
 {
 	char *mode_shell_name = "hsh";
 	char *non_mode_shell_name = "./hsh";
@@ -21,7 +21,7 @@ int not_found(char **arguments, int counter)
 	write(2, ": ", 2);
 	print_numbers(counter);
 	write(2, ": ", 2);
-	write(2, arguments[0], _strlen(arguments[0]));
+	write(2, args[0], _strlen(args[0]));
 	write(2, ": not found\n", 12);
 	return (127);
 }
